6287037907f31397e110160ebeb5540e555c452a
Georgia Martinez <georgiacmartinez@gmail.com>
2026-03-03 14:03:57 -0500

    Wikipedia pages are rendered on the site

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App.tsx";
+import { theme } from "./theme.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </StrictMode>
+);
---
+++ src/theme.ts
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material/styles";
+
+const wikipediaHeadingFont = '"Linux Libertine", Georgia, Times, serif';
+
+export const theme = createTheme({
+  typography: {
+    h1: {
+      fontFamily: wikipediaHeadingFont,
+    },
+    h2: {
+      fontFamily: wikipediaHeadingFont,
+    },
+    h3: {
+      fontFamily: wikipediaHeadingFont,
+    },
+    h4: {
+      fontFamily: wikipediaHeadingFont,
+    },
+    h5: {
+      fontFamily: wikipediaHeadingFont,
+    },
+  },
+});
